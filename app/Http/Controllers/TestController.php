2cfca7e7fe6530eacfeaf7b427d8b5efc6dd2db2
nao <naonao1814naonao@gmail.com>
2021-05-24 15:27:01 +0900

    laravel uiと認証など
    Node.jsのインストールなど

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -4,7 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Test;
+
 class TestController extends Controller
 {
     //
+    public function index()
+    {
+        $values = Test::all();
+
+        //dd($values);
+
+        return view('tests.test', compact('values'));
+    }
 }
